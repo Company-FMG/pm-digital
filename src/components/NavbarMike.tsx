ac827ba71b1ff07bda43ef3e0208d3a2e6b7e52b
Luís <luismsm2003@gmail.com>
2024-05-09 17:03:10 -0300

    WIP: Iniciada responsividade na Navbar

--- src/components/NavbarMike.tsx
+++ src/components/NavbarMike.tsx
@@ -4,15 +4,15 @@ export default function NavbarMike(){
  return(
   <>
   <header className="p-0 bg-bluemike">
-	<div className="container flex justify-between h-32 mx-40">
+	<div className="container flex justify-between h-32 mx-auto px-4">
 		<a href="/" className="flex items-center p-2">
 			<img className="h-20" src={logomike}/>
 		</a>
 		<div className="flex flex-row gap-6 items-center justify-center">
 			<a href="#">
-		 	   <img alt="" className="w-16 rounded-full ring-4 ring-white ring-offset-4 ring-offset-bluemike" src="https://source.unsplash.com/40x40/?portrait?4" />
+		 	   <img alt="" className="w-12 rounded-full ring-4 ring-white ring-offset-4 ring-offset-bluemike" src="https://source.unsplash.com/40x40/?portrait?4" />
       	  </a>
-		<div className="font-poppins text-2xl text-white">
+		<div className="font-poppins text-xl text-white">
         	<p>Rafael Martins</p>
 			<p>COPOM</p>
    		</div>
