d04e60b86df9fcc746ccc996c5d8710337c72c4f
Nicholas <nicholasbergqvist14@gmail.com>
2024-05-15 08:42:51 -0300

    WIP: tela de viaturas em circulação

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import App from "./App";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
+import ComplaintViaturaCirculacao from "./routes/ComplaintViaturaCirculacao";
 
 export default function AppRoutes() {
     return(
@@ -9,6 +10,7 @@ export default function AppRoutes() {
             <Route path="/" element={<App />}/>
             <Route index element={<Home />} />
             <Route path="login" element={<Login/>}/>
+            <Route path="viaturas" element={<ComplaintViaturaCirculacao/>}/>
         </Routes>
     )
 }
---
+++ src/routes/ComplaintViaturaCirculacao.tsx
@@ -0,0 +1,48 @@
+import NavbarMike from "../components/NavbarMike";
+import Viatura from "../components/Viatura";
+export default function ComplaintViaturaCirculacao() {
+  return (
+    <>
+      <NavbarMike />
+      <div className="px-48 py-2">
+        <h1 className="font-bold text-5xl">Viaturas em Circulação</h1>
+        <div className="pt-6">
+          <p className="font-bold text-2xl pb-6">Mais próxima</p>
+          <Viatura
+            idViatura={"2378"}
+            placa={"48621EJF"}
+            responsavel={"Sgt Alencar"}
+            efetivo={"Sgt. Alencar, Sld Marreira, Sld Correia"}
+            areaAtuacao={"Recife"}
+          />
+        </div>
+
+        <p className="font-bold text-2xl pt-6 pb-6">Outras viaturas</p>
+        <div className="">
+          <Viatura 
+            idViatura={"0000"}
+            placa={"aaaaaa"}
+            responsavel={"Sgt Alencar"}
+            efetivo={"Sgt. Alencar, Sld Marreira, Sld Correia"}
+            areaAtuacao={"Recife"}
+            
+          />
+          <Viatura
+            idViatura={"0000"}
+            placa={"aaaaaa"}
+            responsavel={"Sgt Alencar"}
+            efetivo={"Sgt. Alencar, Sld Marreira, Sld Correia"}
+            areaAtuacao={"Recife"}
+          />
+          <Viatura
+            idViatura={"0000"}
+            placa={"aaaaaa"}
+            responsavel={"Sgt Alencar"}
+            efetivo={"Sgt. Alencar, Sld Marreira, Sld Correia"}
+            areaAtuacao={"Recife"}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
