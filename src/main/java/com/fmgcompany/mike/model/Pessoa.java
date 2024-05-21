0a0f8a64c760465c51fe3b6d36031d58d5f6746c
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-21 13:47:27 -0300

    Added methods in class Person

--- src/main/java/com/fmgcompany/mike/model/Pessoa.java
+++ src/main/java/com/fmgcompany/mike/model/Pessoa.java
@@ -1,6 +1,24 @@
-public class abstract Pessoa {
+public abstract class Pessoa {
     protected String nome;
     protected String cpf;
     protected int idade;
     protected String sexo;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
