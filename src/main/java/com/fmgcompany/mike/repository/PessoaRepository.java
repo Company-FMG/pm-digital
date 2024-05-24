9c65a4b5577271f9948f544f93988ce39495a570
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-24 01:43:13 -0300

    Deletada: PessoaRepository, pois a Pessoa é uma classe abstrata

--- src/main/java/com/fmgcompany/mike/repository/PessoaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fmgcompany.mike.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.fmgcompany.mike.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, String>{
-    //sem métodos a mais
-}
