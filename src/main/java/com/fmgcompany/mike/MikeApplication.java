89cccdeb594767c9e841c6f6d0a79e840873bb3c
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-24 00:24:29 -0300

    deletando último commit

--- src/main/java/com/fmgcompany/mike/MikeApplication.java
+++ src/main/java/com/fmgcompany/mike/MikeApplication.java
@@ -1,13 +1,19 @@
 package com.fmgcompany.mike;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MikeApplication {
+public class MikeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MikeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hello");
 	}
 
 }
