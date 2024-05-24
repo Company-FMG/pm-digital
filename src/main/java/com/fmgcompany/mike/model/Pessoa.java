76ba7d4a185ff9d487e5c5793ced7b1b6acf394d
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-24 01:12:37 -0300

    Atualizado: Pessoa.java

--- src/main/java/com/fmgcompany/mike/model/Pessoa.java
+++ src/main/java/com/fmgcompany/mike/model/Pessoa.java
@@ -2,8 +2,10 @@ package com.fmgcompany.mike.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="pessoas")
 public abstract class Pessoa {
     
     @Id
