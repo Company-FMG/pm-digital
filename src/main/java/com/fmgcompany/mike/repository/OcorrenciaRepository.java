3e344d6596d6d190fa329bbc8af0fbddb0362ad2
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-24 17:11:29 -0300

    Atualizado: OcorrenciaRepository.java

--- src/main/java/com/fmgcompany/mike/repository/OcorrenciaRepository.java
+++ src/main/java/com/fmgcompany/mike/repository/OcorrenciaRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fmgcompany.mike.model.Ocorrencia;
 
 public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, String>{
-    
+    //sem métodos adicionais
 }
