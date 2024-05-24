531eac718b7858f17dd02e680d7f6f2faeb491a5
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-24 18:04:05 -0300

    Atualizada: Ocorrencia.java

--- src/main/java/com/fmgcompany/mike/model/Ocorrencia.java
+++ src/main/java/com/fmgcompany/mike/model/Ocorrencia.java
@@ -1,9 +1,29 @@
 package com.fmgcompany.mike.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ocorrencias")
 public class Ocorrencia {
+    @Id
     private String idOcorrencia;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idPolicial", referencedColumnName = "id")
     private Policial responsavelBO;
     private String relatorio;
+
+    public Ocorrencia() {}
+
+    public Ocorrencia(String idOcorrencia, Policial responsavelBO, String relatorio) {
+        this.idOcorrencia = idOcorrencia;
+        this.responsavelBO = responsavelBO;
+        this.relatorio = relatorio;
+    }
 
     //getters
     public String getIdOcorrencia() {
