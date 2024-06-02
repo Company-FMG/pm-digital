480f8682d576db9db0e986e60aeefdeea49bb0ba
louixpng <luismsm2003@gmail.com>
2024-06-02 13:20:52 -0300

    fix: updated App and AppRoutes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ModalProvider } from "./components/ModalContext"
-import App from './App';
+import App from './AppRoutes';
 import './index.css'
 
 const container = document.getElementById('root');
