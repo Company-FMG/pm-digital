135ec7404244fd38a270d1d95c48cc81ef8df911
Nicholas <nicholasbergqvist14@gmail.com>
2024-06-01 22:30:56 -0300

    feat: modal AlertaOcorrencia is now stylized and functional

--- src/components/CreateContext.tsx
+++ src/components/CreateContext.tsx
@@ -2,6 +2,8 @@ import React, { createContext, ReactNode, useState } from "react";
 interface StateContextProps {
   show: boolean;
   handleShow: () => void;
+  isModalOpen: boolean;
+  handleModal: () => void;
 }
 
 export const StateContext = createContext<StateContextProps | undefined>(
@@ -13,14 +15,21 @@ interface StateProviderProps {
 }
 
 export const StateProvider: React.FC<StateProviderProps> = ({ children }) => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleShow = () => {
     setShow(!show);
   };
 
+  const handleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <StateContext.Provider value={{ show, handleShow }}>
+    <StateContext.Provider
+      value={{ show, handleShow, isModalOpen, handleModal }}
+    >
       {children}
     </StateContext.Provider>
   );
