0938dfacd934e4ae0af61c40e846d2ff8cc447fd
LorenzoLeao <lrzo.leao@gmail.com>
2024-06-03 16:46:36 -0300

    "ajustes da tela de login"

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -3,6 +3,7 @@ import MikeLogin from "../assets/MikeLogin.svg"
 import PublicSafety from "../assets/Public Safety.svg"
 import Secure from "../assets/Secure.svg"
 import MikeLogoSlogan  from "../assets/MikeLogoSlogan.svg"
+import { IonInput } from "@ionic/react";
 export default function Login() {
   return (
     
@@ -10,31 +11,31 @@ export default function Login() {
       <img
         loading="lazy"
         src={MikeLogin}
-        className="self-center max-w-full aspect-[0.91] w-[263px]"
+        className="self-center max-w-full aspect-[1.0] w-[263px]"
       />
       <div className="self-center mt-8 text-xl font-medium text-center text-white">
         Um sistema inteligente para a segurança pública
       </div>
-      <div className="flex gap-3.5 mt-14 text-xl text-white whitespace-nowrap">
+      <div className="flex gap-0 mt-14 text-xl text-white whitespace-nowrap">
         <img
           loading="lazy"
             srcSet={PublicSafety}
           className="shrink-0 w-9 aspect-square"
         />
-        <div className="flex-auto my-auto italic">Matrícula</div>
+        <IonInput type="text" className="italic white" placeholder="Matrícula"></IonInput>
       </div>
       <img
         loading="lazy"
         src="https://cdn.builder.io/api/v1/image/assets/TEMP/a78b158e3c8ce9606698491daf89cee5265c1013705139db70921403ab34b2f4?apiKey=91a1d4223fb149a1a197b80828621a6c&"
         className="mt-3 w-full border-2 border-white border-solid aspect-[100] stroke-[2.194px] stroke-white"
       />
-      <div className="flex gap-3.5 self-start mt-12 text-xl text-white whitespace-nowrap">
+      <div className="flex gap-0 self-start mt-12 text-xl text-white whitespace-nowrap">
         <img
           loading="lazy"
-          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/e7b49bad0bb65cb8148865cba46aff7691379693a59ddf0d0524a460fa93f73a?apiKey=91a1d4223fb149a1a197b80828621a6c&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/e7b49bad0bb65cb8148865cba46aff7691379693a59ddf0d0524a460fa93f73a?apiKey=91a1d4223fb149a1a197b80828621a6c&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e7b49bad0bb65cb8148865cba46aff7691379693a59ddf0d0524a460fa93f73a?apiKey=91a1d4223fb149a1a197b80828621a6c&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/e7b49bad0bb65cb8148865cba46aff7691379693a59ddf0d0524a460fa93f73a?apiKey=91a1d4223fb149a1a197b80828621a6c&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/e7b49bad0bb65cb8148865cba46aff7691379693a59ddf0d0524a460fa93f73a?apiKey=91a1d4223fb149a1a197b80828621a6c&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e7b49bad0bb65cb8148865cba46aff7691379693a59ddf0d0524a460fa93f73a?apiKey=91a1d4223fb149a1a197b80828621a6c&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/e7b49bad0bb65cb8148865cba46aff7691379693a59ddf0d0524a460fa93f73a?apiKey=91a1d4223fb149a1a197b80828621a6c&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/e7b49bad0bb65cb8148865cba46aff7691379693a59ddf0d0524a460fa93f73a?apiKey=91a1d4223fb149a1a197b80828621a6c&"
-          className="shrink-0 w-9 aspect-[0.97]"
+            srcSet={Secure}
+          className="shrink-0 w-9 aspect-[0.99]"
         />
-        <div className="my-auto italic">Senha</div>
+         <IonInput type="password" className="italic white" placeholder="Senha"></IonInput>
       </div>
       <div className="flex overflow-hidden relative flex-col justify-center mt-3 border-2 border-white border-solid aspect-[161.5] stroke-[2.194px] stroke-white">
         <img
