635454eeeba9e6108c852352751afadc686773f7
LorenzoLeao <lrzo.leao@gmail.com>
2024-06-04 14:12:40 -0300

    responsividade ajustada

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 import MikeLogin from "../assets/MikeLogin.svg"
 import PublicSafety from "../assets/Public Safety.svg"
 import Secure from "../assets/Secure.svg"
-import MikeLogoSlogan  from "../assets/MikeLogoSlogan.svg"
-import { IonButton, IonContent, IonInput, IonPage } from "@ionic/react";
+import MikeLogoSlogan from "../assets/MikeLogoSlogan.svg"
+import { IonButton, IonCol, IonContent, IonHeader, IonImg, IonInput, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
 export default function Login() {
   return (
-    <IonPage>
-    <IonContent>
-    <div className="flex flex-col px-14 py-20 mx-auto w-full bg-blue-700 max-w-[480px]">
+    <IonPage className="margin-color-blue-700">
+    <IonContent >
+    <div className="flex flex-col px-14 py-20 mx-auto w-full bg-blue-700 w-[480px]">
       <img
         loading="lazy"
         src={MikeLogin}
@@ -17,49 +17,35 @@ export default function Login() {
       <div className="self-center mt-8 text-xl font-medium text-center text-white">
         Um sistema inteligente para a segurança pública
       </div>
-
-
       <div className="flex gap-0 mt-14 text-xl text-white whitespace-nowrap">
         <img
           loading="lazy"
             srcSet={PublicSafety}
           className="shrink-0 w-9 aspect-square"
         />
-        <IonInput color="light"type="text" className="italic white" placeholder="Matrícula"></IonInput>
+        <IonInput type="text" className="italic white" placeholder="Matrícula"></IonInput>
       </div>
      
-      <div className="flex gap-0  mt-14 text-xl text-white whitespace-nowrap">
+      <div className="flex gap-0 self-start mt-12 text-xl text-white whitespace-nowrap">
         <img
           loading="lazy"
             srcSet={Secure}
-          className="shrink-0 w-9 aspect-square"
+          className="shrink-0 w-9 aspect-[0.99]"
         />
-         <IonInput color="light" type="password" className="italic white" placeholder="Senha"></IonInput>
+         <IonInput type="password" className="italic white" placeholder="Senha"></IonInput>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <button className="self-end mt-5 pb-12 text-base italic text-white underline">
+    
+      <button className="self-end mt-5 text-base italic text-white underline">
         Esqueci minha senha
       </button>
-      <IonButton href="/" className="text-center italic text-xl p-35 rounded-xl h-14 w-73 bg-white" color="primary" fill="outline">
+      <button className="justify-center items-center px-16 py-5 mt-7 text-xl font-bold text-blue-700 whitespace-nowrap bg-white rounded-md" >
         Entrar
-      </IonButton>
+      </button>
     </div>
     </IonContent>
     </IonPage>
   );
-  
+
 }
 
 
