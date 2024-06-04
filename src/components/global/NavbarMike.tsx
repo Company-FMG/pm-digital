22501f7a01b4fd9a17cd041151666f26f4f62728
louixpng <luismsm2003@gmail.com>
2024-06-03 23:45:11 -0300

    minor changes

--- src/components/global/NavbarMike.tsx
+++ src/components/global/NavbarMike.tsx
@@ -9,7 +9,7 @@ export default function NavbarMike () {
                         <img className="h-8 sm:h-8 md:h-16 lg:h-20" src={logomike} />
                     </a>
                     <div className="flex flex-row gap-4 sm:gap-6 items-center justify-center">
-                        <a>
+                        <a href="/perfil">
                             <img alt="" className="w-6 sm:w-8 md:w-12 lg:w-12 rounded-full ring-2 md:ring-4 ring-white ring-offset-4 ring-offset-bluemike" src="https://source.unsplash.com/40x40/?portrait?4" />
                         </a>
                         <div className="font-poppins text-xs sm:text-base md:text-xl lg:text-2xl text-white">
