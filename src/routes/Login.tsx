6cbe850ee6f616feb0e0b67741f551e6121ffc93
LorenzoLeao <lrzo.leao@gmail.com>
2024-06-04 00:54:41 -0300

    ajustes tela login

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -3,10 +3,11 @@ import MikeLogin from "../assets/MikeLogin.svg"
 import PublicSafety from "../assets/Public Safety.svg"
 import Secure from "../assets/Secure.svg"
 import MikeLogoSlogan  from "../assets/MikeLogoSlogan.svg"
-import { IonInput } from "@ionic/react";
+import { IonContent, IonInput, IonPage } from "@ionic/react";
 export default function Login() {
   return (
-    
+    <IonPage>
+    <IonContent>
     <div className="flex flex-col px-14 py-20 mx-auto w-full bg-blue-700 max-w-[480px]">
       <img
         loading="lazy"
@@ -16,39 +17,38 @@ export default function Login() {
       <div className="self-center mt-8 text-xl font-medium text-center text-white">
         Um sistema inteligente para a segurança pública
       </div>
+
+
       <div className="flex gap-0 mt-14 text-xl text-white whitespace-nowrap">
         <img
           loading="lazy"
             srcSet={PublicSafety}
           className="shrink-0 w-9 aspect-square"
         />
-        <IonInput type="text" className="italic white" placeholder="Matrícula"></IonInput>
+        <IonInput color="light"type="text" className="italic white" placeholder="Matrícula"></IonInput>
       </div>
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/a78b158e3c8ce9606698491daf89cee5265c1013705139db70921403ab34b2f4?apiKey=91a1d4223fb149a1a197b80828621a6c&"
-        className="mt-3 w-full border-2 border-white border-solid aspect-[100] stroke-[2.194px] stroke-white"
-      />
-      <div className="flex gap-0 self-start mt-12 text-xl text-white whitespace-nowrap">
+     
+      <div className="flex gap-0  mt-14 text-xl text-white whitespace-nowrap">
         <img
           loading="lazy"
             srcSet={Secure}
           className="shrink-0 w-9 aspect-[0.99]"
         />
-         <IonInput type="password" className="italic white" placeholder="Senha"></IonInput>
+         <IonInput color="light" type="password" className="italic white" placeholder="Senha"></IonInput>
       </div>
-      <div className="flex overflow-hidden relative flex-col justify-center mt-3 border-2 border-white border-solid aspect-[161.5] stroke-[2.194px] stroke-white">
-        <img
-          loading="lazy"
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/a78b158e3c8ce9606698491daf89cee5265c1013705139db70921403ab34b2f4?apiKey=91a1d4223fb149a1a197b80828621a6c&"
-          className="object-cover absolute inset-0 size-full"
-        />
-        <img
-          loading="lazy"
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/a78b158e3c8ce9606698491daf89cee5265c1013705139db70921403ab34b2f4?apiKey=91a1d4223fb149a1a197b80828621a6c&"
-          className="w-full border-2 border-white border-solid aspect-[100] stroke-[2.194px] stroke-white"
-        />
-      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
       <button className="self-end mt-5 text-base italic text-white underline">
         Esqueci minha senha
       </button>
@@ -56,7 +56,11 @@ export default function Login() {
         Entrar
       </button>
     </div>
+    </IonContent>
+    </IonPage>
   );
+  
 }
+
 
 
