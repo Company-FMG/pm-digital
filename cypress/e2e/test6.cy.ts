275b0fa103e32b6239206c984751aeecc83f17f2
ialeAlmeida <almeidaialesantos@gmail.com>
2024-06-09 19:56:50 -0300

    updated: test5 -> agora, o test6 verifica a saída da rota perfil de volta pra home

--- cypress/e2e/test6.cy.ts
+++ cypress/e2e/test6.cy.ts
@@ -1,15 +1,11 @@
-describe('Modal "SobreMike"', () => {
-    it('deve abrir o modal e verificar seu conteúdo', () => {
+import { should } from "chai";
+
+describe('Perfil para Home', () => {
+    it('testando a saída de perfil para a home', () => {
+        cy.visit('/perfil');
+        
+        cy.contains('VOLTAR PARA PÁGINA INICIAL').should('be.visible');
+        
         cy.visit('/home');
-
-        cy.get(':nth-child(3) > .text-center').click();
-
-        cy.get(':nth-child(2) > .lg:flex > .rounded-xl');
-
-        cy.get('.w-72');
-        cy.get('.font-bold').contains('Sobre o Mike').should('be.visible');
-        //.w-72 > .text-xl
-        cy.get('.w-72 > .text-xl').contains('Tecnologia inovadora para facilitar denúncias e agilizar ações de prevenção a violência e situações emergentes. Desenvolvedores resposáveis: - Ana Carolyne Costa - Iale Almeida - Lorenzo Leão - Luís Silvestre - Nicholas Bergqvist - Rafael Figuerôa').should('be.visible');
-        //'.w-72 > .text-xl'
     });
 });
