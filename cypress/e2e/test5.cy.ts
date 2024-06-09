82f6cb2f1e789fba29234b5b69698a294f9ab0ec
ialeAlmeida <almeidaialesantos@gmail.com>
2024-06-09 16:32:01 -0300

    update: test5

--- cypress/e2e/test5.cy.ts
+++ cypress/e2e/test5.cy.ts
@@ -2,7 +2,7 @@ describe('Modal "SobreMike"', () => {
     it('verifica abertura do modal', () => {
         cy.visit('/home');
 
-        cy.get('button.open-sobre-mike-modal').click();
+        cy.get('button').should('Sobre o Mike').click();
 
         cy.get('div.modal-content').should('be.visible');
     });
