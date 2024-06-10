22af5959bfa5cd0f5df183c7a8da7ef763309885
Iale Almeida <143238811+ialeAlmeida@users.noreply.github.com>
2024-06-09 22:00:04 -0300

    Delete cypress/e2e/test7.cy.ts

--- cypress/e2e/test7.cy.ts
+++
@@ -1,8 +0,0 @@
-// import Button from '/home'
-
-// describe('Sobre o Mike', () => {
-//     it('uses custom text for the button label', () => {
-//         cy.visit(/home)
-//         cy.get('button').should('contains.text', 'Click me!')
-//       })
-// }
