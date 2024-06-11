58bfbbdcad23b7cd1491cb74250852a97a03ff0a
RafaFigueiroa <rafatinsfig@gmail.com>
2024-06-11 14:24:55 -0300

    implementing the auth layout

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,20 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import App from "./App";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
 import ComplaintViaturaCirculacao from "./routes/ComplaintViaturaCirculacao";
 import ComplaintRegister from "./routes/ComplaintRegister";
 import Complaint from "./routes/Complaint";
+import AuthenticatedLayout from "./layouts/AuthenticatedLayout";
 
 export default function AppRoutes() {
     return(
         <Routes>
-            <Route path="/" element={<App/>}/>
-            <Route index element={<Home/>} />
-            <Route path="login" element={<Login/>}/>
-            <Route path="viaturas" element={<ComplaintViaturaCirculacao/>}/>
-            <Route path="newcomplaint" element={<ComplaintRegister/>}/>
-            <Route path="denuncia" element={<Complaint/>}/>
+            <Route path="/" element={<Login/>}/>
+            <Route element={<AuthenticatedLayout />}>
+                <Route path="home" element={<Home/>} />
+                <Route path="viaturas" element={<ComplaintViaturaCirculacao/>}/>
+                <Route path="newcomplaint" element={<ComplaintRegister/>}/>
+                <Route path="denuncia" element={<Complaint/>}/>
+            </Route>
         </Routes>
     )
 }
