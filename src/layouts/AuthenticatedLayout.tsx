04ada6ad2689737ffb42db26e10b4b61864cf29c
RafaFigueiroa <rafatinsfig@gmail.com>
2024-06-11 14:22:50 -0300

    feat: creating a layout for authenticated users

---
+++ src/layouts/AuthenticatedLayout.tsx
@@ -0,0 +1,30 @@
+import NavbarMike from '../components/global/NavbarMike';
+import ProvisoryNavigationBreadcrumb from '../components/global/ProvisoryNavigationBreadcrumb';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+//implementar a verificação de autenticação aqui
+const useAuth = () => {
+  const user = localStorage.getItem('user');
+  return user !== null;
+};
+
+function AuthenticatedLayout() {
+  const navigate = useNavigate();
+  const isAuthenticated = useAuth();
+
+  if (!isAuthenticated) {
+    navigate('/login');
+  }
+
+  return (
+    <>
+      <NavbarMike />
+      <ProvisoryNavigationBreadcrumb />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default AuthenticatedLayout;
