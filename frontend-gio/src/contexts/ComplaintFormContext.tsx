1764275a3937fb6c3132e5538a29e252799d9ae8
louixpng <luismsm2003@gmail.com>
2024-10-24 21:13:02 -0300

    feat: created FormContext

---
+++ frontend-gio/src/contexts/ComplaintFormContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState } from "react";
+
+interface ComplaintFormContextProps {
+    formData: FormData;
+    setFormData: React.Dispatch<React.SetStateAction<FormData>>;
+}
+
+interface FormProviderProps {
+    children: React.ReactNode;
+}
+
+interface FormData {
+    tipo: string;
+    local: string;
+    cep: string | undefined;
+    relato: string;
+    geolocation: {lat: number, lng: number} | null;
+}
+
+const FormContext = createContext<ComplaintFormContextProps | undefined>(undefined)
+
+export const FormProvider = ({children}:FormProviderProps) => {
+    const [formData, setFormData] = useState<FormData>({
+        local: "",
+        cep: undefined,
+        relato: "",
+        tipo: "",
+        geolocation: null,
+    });
+
+    return (
+        <FormContext.Provider value={{ formData, setFormData }}>
+            {children}
+        </FormContext.Provider>
+    )
+}
+
+export const useForm = () => {
+    const context = useContext(FormContext)
+    if (!context) {
+        throw new Error("useForm must be used within a FormProvider")
+    }
+    return context;
+}
