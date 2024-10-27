96b608d8bf31fa003de9c098f2cdafdd1d3e9f98
louixpng <luismsm2003@gmail.com>
2024-10-26 23:18:44 -0300

    fix: minor style changes on Map

--- frontend-gio/src/components/complaint/ComplaintMap.tsx
+++ frontend-gio/src/components/complaint/ComplaintMap.tsx
@@ -3,34 +3,34 @@ const api_key = import.meta.env.VITE_REACT_GOOGLE_MAPS_KEY
 
 const center = { lat: -8.054895, lng: -34.887762 }
 
-export default function ComplaintMap () {
-    const endereco = 'Rua General Viriato de Medeiros';
-    const { isLoaded } = useJsApiLoader({
-      googleMapsApiKey: api_key,
-      libraries: ['places']
+export default function ComplaintMap() {
+  const endereco = 'Rua General Viriato de Medeiros';
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: api_key,
+    libraries: ['places']
   })
 
   if (!isLoaded) {
-      return <h1>Erro</h1>
+    return <h1>Erro</h1>
   }
-    
-    return (
-        <>
-        <div>
-              <h1 className="text-sm md:text-xl lg:text-2xl font-bold">
-                Localização: {endereco}
-              </h1>
-              <div className="flex mx-auto mt-12 w-3/4 md:w-full h-[600px] rounded-lg border-4 border-bluemike">
-              <GoogleMap 
-                    mapContainerStyle={{ width: '100%', height: '100%',}}
-                    zoom={15}
-                    center={center}
-                    options={{ mapId: "3dbe8233f2b40a5c" }}
-                >
-                    <Marker position={center}></Marker>
-                </GoogleMap>
-                </div>
-              </div>
-        </>
-    )
+
+  return (
+    <>
+      <div className="">
+        <h1 className="text-sm md:text-xl lg:text-2xl font-bold">
+          Localização: {endereco}
+        </h1>
+        <div className="flex mt-4 w-full h-map-mdH rounded-lg border-4 border-bluemike">
+          <GoogleMap
+            mapContainerStyle={{ width: '100%', height: '100%', }}
+            zoom={15}
+            center={center}
+            options={{ mapId: "3dbe8233f2b40a5c" }}
+          >
+            <Marker position={center}></Marker>
+          </GoogleMap>
+        </div>
+      </div>
+    </>
+  )
 }
