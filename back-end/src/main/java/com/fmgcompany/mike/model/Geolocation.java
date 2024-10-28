ae11b97e8bdf4e1f7e9d8a894bfff0962a568da6
louixpng <luismsm2003@gmail.com>
2024-10-27 22:30:26 -0300

    feat: created Geolocation model

---
+++ back-end/src/main/java/com/fmgcompany/mike/model/Geolocation.java
@@ -0,0 +1,25 @@
+package com.fmgcompany.mike.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="geolocations")
+public class Geolocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private double lat;
+    private double lng;
+
+    @OneToOne(mappedBy = "geolocation")
+    private Denuncia denuncia;
+}
