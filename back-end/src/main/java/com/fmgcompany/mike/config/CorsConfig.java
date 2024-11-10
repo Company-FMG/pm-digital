6e931f2a0fe2f54f7f4d5056007e38313d44c7e3
RafaFigueiroa <rafatinsfig@gmail.com>
2024-11-10 11:27:45 -0300

    update: adding the gio deploy URL to CORS allowed origins

--- back-end/src/main/java/com/fmgcompany/mike/config/CorsConfig.java
+++ back-end/src/main/java/com/fmgcompany/mike/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:8100")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:8100", "https://gio-pm-digital.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
