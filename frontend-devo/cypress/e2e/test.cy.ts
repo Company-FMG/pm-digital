488d11bb14ce59d73fa730b0ae4af5b8546e9171
louixpng <luismsm2003@gmail.com>
2024-11-12 17:54:29 -0300

    fix: removed unnecessary dependencies and test files

--- frontend-devo/cypress/e2e/test.cy.ts
+++
@@ -1,5 +0,0 @@
-describe('route test', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-  })
-})
--- frontend-devo/cypress/e2e/test9.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('modal opcoes', () => {
-    it('verifica abertura do modal', () => {
-        cy.visit('/home');
-
-        cy.get('.flex-row > .flex').click();
-
-        cy.get('.top-3').should('be.visible');
-    });
-});
