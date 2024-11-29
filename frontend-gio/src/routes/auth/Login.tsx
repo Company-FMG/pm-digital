49304d13f7d647f75c74c9eaa19943a4ea0f80fe
louixpng <luismsm2003@gmail.com>
2024-11-29 14:07:00 -0300

    fix: fixed responsiveness in Login screen

--- frontend-gio/src/routes/auth/Login.tsx
+++ frontend-gio/src/routes/auth/Login.tsx
@@ -57,48 +57,48 @@ export default function Login() {
             className="hidden lg:block object-cover lg:w-[36rem] xl:w-[48rem] 2xl:w-[65rem] h-screen brightness-75"
           />
         </section>
-        <section className="flex flex-col mx-auto items-end text-end justify-center text-white">
-          <div className="w-full max-w-md 2xl:max-w-lg px-4 lg:px-16 2xl:px-12 mx-auto my-auto pb-10">
+        <section className="flex flex-col mx-auto my-auto items-end text-end justify-center text-white">
+          <div className="flex flex-col gap-4 2xl: gap-8">
             <Image
               src={PMDigitalcomSlogan}
               alt="Logo"
-               className="w-48 sm:w-56 md:w-64 lg:w-72 xl:w-80 2xl:w-96 max-w-full mx-auto pt-10 pb-16"
+               className="w-64 sm:w-72 md:w-64 xl:w-72 2xl:w-96 max-w-full mx-auto"
             />
-            <form className="" onSubmit={handleLogin}>
+            <form className="flex flex-col gap-5 2xl:gap-6" onSubmit={handleLogin}>
 
-              <div className="mb-5 flex items-center gap-3">
+              <div className="flex items-center gap-3">
                 <img src={PublicSafety} alt="Matrícula" className="w-7 h-7" />
                 <input
                   type="text"
-                  className="mt-2 p-3 w-full border-b bg-transparent text-white italic"
+                  className="p-3 w-full border-b bg-transparent text-white italic"
                   placeholder="Matrícula"
                   value={matricula}
                   onChange={(e) => setMatricula(e.target.value)}
                 />
               </div>
 
-              <div className="mb-5 flex items-center gap-3">
+              <div className="flex items-center gap-3">
                 <img src={Secure} alt="Senha" className="w-7 h-7" />
                 <input
                   type="password"
-                  className="mt-1 p-3 w-full border-b bg-transparent text-white italic"
+                  className="p-3 w-full border-b bg-transparent text-white italic"
                   placeholder="Senha"
                   value={senha}
                   onChange={(e) => setSenha(e.target.value)}
                 />
               </div>
 
-              <a href="#" className="text-xl italic underline">
+              <a href="#" className="text-md italic underline">
                 Esqueci minha senha
               </a>
 
               <button
                 type="submit"
-                className="mt-8 p-4 w-full bg-white text-blue-700 text-2xl font-bold rounded-lg">
+                className="p-3 w-full bg-white text-blue-700 text-xl font-bold rounded-lg">
                 Entrar
               </button>
-              <div className="text-center mt-4">
-                <a href="/register" className="text-xl italic underline">Primeiro acesso? Começe aqui</a>
+              <div className="text-center">
+                <a href="/register" className="text-md italic underline">Primeiro acesso? Começe aqui</a>
               </div>
             </form>
           </div>
